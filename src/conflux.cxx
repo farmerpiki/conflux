a12b6b8915338ea8d1b64d36e55f4ca77607a8f3
Radu <kitanatahu@gmail.com>
2026-05-13 01:22:11 +0000

    template component split

--- src/conflux.cxx
+++ src/conflux.cxx
@@ -5,6 +5,8 @@ export import conflux.types;
 #if CONFLUX_HAS_FILE_WATCH
 export import conflux.file_watch;
 #endif
+#if CONFLUX_HAS_TEMPLATES
 export import conflux.templates;
+#endif
 export import conflux.json;
 export import conflux.net.http;
