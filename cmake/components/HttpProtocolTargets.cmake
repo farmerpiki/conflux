cdfcda549c95bad5d055baba30fd718cd72bc5c6
Radu <kitanatahu@gmail.com>
2026-06-02 19:23:49 +0000

    Extract HTTP protocol CMake targets

---
+++ cmake/components/HttpProtocolTargets.cmake
@@ -0,0 +1,79 @@
+if(CONFLUX_WANT_HTTP_CORE OR CONFLUX_WANT_HTTP_SERVER)
+add_library(conflux_http1 STATIC)
+target_sources(conflux_http1
+    PUBLIC FILE_SET CXX_MODULES
+        BASE_DIRS "${CONFLUX_SRC_ROOT}"
+        FILES ${CONFLUX_SRC_ROOT}/net/http1_parser.cxx
+)
+target_link_libraries(conflux_http1
+    PRIVATE conflux_options
+    PUBLIC  conflux_types
+    PUBLIC  conflux_http_core
+    PUBLIC  conflux_net_config
+)
+endif() # CONFLUX_WANT_HTTP_CORE || CONFLUX_WANT_HTTP_SERVER (HTTP/1 parser)
+
+if(CONFLUX_WANT_HTTP_SERVER AND CONFLUX_HAS_HTTP2 STREQUAL "true")
+add_library(conflux_http2 STATIC)
+target_sources(conflux_http2
+    PUBLIC FILE_SET CXX_MODULES
+        BASE_DIRS "${CONFLUX_SRC_ROOT}"
+        FILES ${CONFLUX_SRC_ROOT}/net/http2.cxx
+)
+target_link_libraries(conflux_http2
+    PRIVATE conflux_options
+    PUBLIC  conflux_types
+    PUBLIC  OpenSSL::SSL
+    PUBLIC  OpenSSL::Crypto
+    PUBLIC  PkgConfig::NGHTTP2
+)
+target_compile_definitions(conflux_http2 PUBLIC CONFLUX_HAS_HTTP2=1)
+endif() # CONFLUX_WANT_HTTP_SERVER && HTTP/2 available
+
+if(CONFLUX_WANT_HTTP_SERVER AND CONFLUX_HAS_HTTP3 STREQUAL "true")
+add_library(conflux_http3 STATIC)
+target_sources(conflux_http3
+    PUBLIC FILE_SET CXX_MODULES
+        BASE_DIRS "${CONFLUX_SRC_ROOT}"
+        FILES ${CONFLUX_SRC_ROOT}/net/http3.cxx
+)
+target_link_libraries(conflux_http3
+    PRIVATE conflux_options
+    PUBLIC  conflux_types
+    PUBLIC  conflux_net_config
+    PUBLIC  conflux_http_core
+    PUBLIC  conflux_http_router
+    PUBLIC  OpenSSL::SSL
+    PUBLIC  OpenSSL::Crypto
+    PUBLIC  PkgConfig::NGTCP2
+    PUBLIC  PkgConfig::NGTCP2_CRYPTO_OSSL
+    PUBLIC  PkgConfig::NGHTTP3
+)
+target_compile_definitions(conflux_http3 PUBLIC CONFLUX_HAS_HTTP3=1)
+endif() # CONFLUX_WANT_HTTP_SERVER && HTTP/3 available
+
+if(CONFLUX_WANT_HTTP_SERVER)
+add_library(conflux_http_protocol STATIC)
+target_sources(conflux_http_protocol
+    PUBLIC FILE_SET CXX_MODULES
+        BASE_DIRS "${CONFLUX_SRC_ROOT}"
+        FILES ${CONFLUX_SRC_ROOT}/net/http_protocol.cxx
+)
+target_link_libraries(conflux_http_protocol
+    PRIVATE conflux_options
+    PUBLIC  conflux_types
+    PUBLIC  conflux_http1
+)
+if(TARGET conflux_http2)
+    target_link_libraries(conflux_http_protocol PUBLIC conflux_http2)
+    target_compile_definitions(conflux_http_protocol PUBLIC CONFLUX_HAS_HTTP2=1)
+else()
+    target_compile_definitions(conflux_http_protocol PUBLIC CONFLUX_HAS_HTTP2=0)
+endif()
+if(TARGET conflux_http3)
+    target_link_libraries(conflux_http_protocol PUBLIC conflux_http3)
+    target_compile_definitions(conflux_http_protocol PUBLIC CONFLUX_HAS_HTTP3=1)
+else()
+    target_compile_definitions(conflux_http_protocol PUBLIC CONFLUX_HAS_HTTP3=0)
+endif()
+endif() # CONFLUX_WANT_HTTP_SERVER (HTTP protocol umbrella)
