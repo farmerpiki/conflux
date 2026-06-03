dd455c8207555c06001d2adf6da766f88bbae705
Radu <kitanatahu@gmail.com>
2026-06-03 21:31:08 +0000

    Split metrics E2E tests

--- tests/HttpObservabilityTests.cmake
+++ tests/HttpObservabilityTests.cmake
@@ -1,4 +1,19 @@
 if(TARGET conflux_http_observability)
+    add_executable(conflux_http_metrics_e2e)
+    target_sources(conflux_http_metrics_e2e
+        PRIVATE
+            http_metrics_e2e.cxx
+        PRIVATE FILE_SET CXX_MODULES FILES
+            support.cxx
+    )
+    target_include_directories(conflux_http_metrics_e2e PRIVATE "${CMAKE_SOURCE_DIR}/src/net")
+    target_link_libraries(conflux_http_metrics_e2e
+        PRIVATE
+            conflux
+            conflux_options
+            Catch2::Catch2WithMain
+    )
+
     conflux_add_compile_fail_test(
         TARGET conflux_http_middleware_compile_fail_global_request_id_options
         SOURCE http_middleware_compile_fail_global_request_id_options.cxx
