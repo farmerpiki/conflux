f56c09d9a3f74256412cc708f3815ed76f0d83cb
Radu <kitanatahu@gmail.com>
2026-06-04 03:26:12 +0000

    Trim duplicate extended HTTP reexports

--- src/facade/conflux_extended.cxx
+++ src/facade/conflux_extended.cxx
@@ -34,54 +34,8 @@ export import conflux.json.reflect_provider;
 export import conflux.net.config;
 #endif
 
-#if CONFLUX_SURFACE_HAS_HTTP_CLIENT
-export import conflux.net.http.client;
-#endif
-
 #if CONFLUX_SURFACE_HAS_HTTP_APP
 export import conflux.http.extended;
-#endif
-
-#if CONFLUX_SURFACE_HAS_HTTP_AUTH
-export import conflux.net.auth;
-#endif
-
-#if CONFLUX_SURFACE_HAS_HTTP_POLICY
-export import conflux.net.cors;
-export import conflux.net.rate_limit;
-export import conflux.net.security;
-export import conflux.net.forwarded;
-export import conflux.net.request_id;
-export import conflux.net.ip_filter;
-export import conflux.net.cache_control;
-export import conflux.net.trailing_slash;
-export import conflux.net.redirect;
-export import conflux.net.cookie_signing;
-export import conflux.net.csrf;
-export import conflux.net.etag;
-export import conflux.net.response_cache;
-#endif
-
-#if CONFLUX_SURFACE_HAS_HTTP_OBSERVABILITY
-export import conflux.net.observability;
-export import conflux.net.structured_log;
-export import conflux.net.tracing;
-#endif
-
-#if CONFLUX_SURFACE_HAS_HTTP_METRICS
-export import conflux.net.metrics;
-#endif
-
-#if CONFLUX_SURFACE_HAS_HTTP_OPENAPI
-export import conflux.net.openapi;
-#endif
-
-#if CONFLUX_SURFACE_HAS_HTTP_VHOST
-export import conflux.net.vhost;
-#endif
-
-#if CONFLUX_SURFACE_HAS_TLS_JWT
-export import conflux.net.jwt;
 #endif
 
 #if CONFLUX_SURFACE_HAS_CRYPTO
